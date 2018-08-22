bd085a75a6f0ec34f1372f94bcafee7bd18c726f
ulgoon <ulgoon89@gmail.com>
2018-08-22 13:59:49 +0900

    add socket client

--- resources/socket/socket_server.py
+++ resources/socket/socket_server.py
@@ -18,20 +18,14 @@ if __name__ == '__main__':
         print("Connected: {}".format(addr))
         while True:
             data = client_socket.recv(buffer_size)
-            if not data or data == 'EXIT':
+            if not data or data.decode('utf-8') == 'EXIT':
                 break
             print("msg from client: {}".format(data.decode('utf-8')))
             to_send = time.ctime()
             print("sending client to: {}".format(to_send))
-
             try:
                 client_socket.send(bytes(to_send, 'utf-8'))
             except:
                 print("send failed")
-
         client_socket.close()
     server_socket.close()
-
-
-
-
