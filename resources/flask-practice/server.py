949ed38aa027c28fc6b133a39bad969b82e4e128
ulgoon <ulgoon89@gmail.com>
2019-02-28 18:26:41 +0900

    flask update

--- resources/flask-practice/server.py
+++ resources/flask-practice/server.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import sqlite3 as lite
 
 
 app = Flask(__name__)
@@ -7,10 +8,38 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/users')
-def users():
-    return render_template('users.html')
+@app.route('/users', methods=['GET','POST'])
+def users(msg="Failed"):
+    if request.method == 'GET':
+        conn = lite.connect('./data/users.db')
+        conn.row_factory = lite.Row
+        cur = conn.cursor()
 
+        cur.execute('SELECT * FROM users;')
+        rows = cur.fetchall()
+        conn.close()
+        print(rows)
+        return render_template('users.html', rows=rows)
+    elif request.method == 'POST':
+        try:
+            input_username = request.form["username"]
+            input_password = request.form["userpw"]
+            input_gender = request.form["gender"]
+            input_email = request.form["usermail"]
+            print(input_username)
+            with lite.connect('./data/users.db') as conn:
+                cur = conn.cursor()
+                cur.execute("""
+                        INSERT INTO users(username, password, gender, email)
+                        VALUES(?,?,?,?)
+                        """, (input_username, input_password, input_gender, input_email))
+                conn.commit()
+                msg = "Success"
+        except:
+            conn.rollback()
+            msg = "Failed"
+        finally:
+            return render_template("users.html", msg=msg)
 #/users => "This is user list"
 @app.route('/user/<string:name>')
 def user(name=None):
